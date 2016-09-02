7dd1cb2964c37ccf0fae063a910a6062aba0c62d
Habib <bsr.anwar@outlook.com>
2016-09-02 07:37:35 +0500

    Add filip/whoops Error exception package

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,5 +1,26 @@
 <?php
 
+namespace Application;
 
-echo 'hello world';
+require __DIR__ .'/../vendor/autoload.php';
 
+error_reporting(E_ALL);
+
+$env = 'development';
+
+
+$whoops = new \Whoops\Run;
+
+if($env === 'development') {
+		
+	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+
+	$whoops->pushHandler( function($e) {
+		echo ' Friendly error page and send an email to developer';
+	});
+}
+
+$whoops->register();
+
+throw new \Exception();
